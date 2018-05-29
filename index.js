ae8aff8ad2bc059ddec90d7212fdd9106ea83f46
Spencer <spencer@frozor.io>
2018-05-29 16:03:56 -0400

    add entry point file

---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports = {
+   FlowdockClient: require('./lib/client/FlowdockClient'),
+   Message: require('./lib/structures/Message'),
+   MessageBuilder: require('./lib/structures/MessageBuilder'),
+   MessageType: require('./lib/enum/MessageType')
+};
