09235e03f18092fb530be90485446c78895f9854
Spencer <spencer@frozor.io>
2018-05-31 12:28:25 -0400

    more exports

--- index.js
+++ index.js
@@ -2,5 +2,8 @@ module.exports = {
    FlowdockClient: require('./lib/client/FlowdockClient'),
    Message: require('./lib/structures/Message'),
    MessageBuilder: require('./lib/structures/MessageBuilder'),
-   MessageType: require('./lib/enum/MessageType')
+   MessageType: require('./lib/enum/MessageType'),
+   User: require('./lib/structures/User'),
+   Organization: require('./lib/structures/Organization'),
+   Flow: require('./lib/structures/Flow')
 };
